6745f0c548547f2243fedc978ce24d21fb2e0cb5
Minh Nguyen <ngmikeng@gmail.com>
2019-11-27 07:24:21 -0600

    Add simple ngrx tut counter

---
+++ angular-ngrx-tut-counter/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter/counter.reducer';
+import { CounterComponent } from './counter/counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
